5a6524d9db285e82161dd9cc0c6c423f3db84236
Afej <afekurojoshua@gmail.com>
2020-06-11 22:49:10 +0100

    still working on responsiveness

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+// Sticky menu background
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 150) {
+    document.querySelector('#main-nav').style.opacity = 0.9;
+  } else {
+    document.querySelector('#main-nav').style.opacity = 1;
+  }
+});
+
+//nav bar
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-menu');
+const links = document.querySelectorAll('.nav-menu li');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+  links.forEach((link) => {
+    link.classList.toggle('fade');
+  });
+});
+
 // Smooth Scrolling
 $('#main-nav a, .btn').on('click', function (event) {
   if (this.hash !== '') {
@@ -7,7 +28,7 @@ $('#main-nav a, .btn').on('click', function (event) {
 
     $('html, body').animate(
       {
-        scrollTop: $(hash).offset().top - 20,
+        scrollTop: $(hash).offset().top - 80,
       },
       800
     );
